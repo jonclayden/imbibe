b0307c21738454309af033d5b2996a7c5a64a997
Jon Clayden <code@clayden.org>
2020-09-23 20:24:14 +0100

    Fleshing out main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,89 @@
-#include <R.h>
-#include <Rdefines.h>
-
-#define RNIFTI_NIFTILIB_VERSION 2
-#include "RNifti.h"
-#include "RNiftiAPI.h"
-
 #include "core32.h"
 #include "core64.h"
+#include "RNifti_shim.h"
 
-SEXP run (SEXP _args, SEXP _precision)
+RcppExport SEXP run (SEXP _args, SEXP _precision)
 {
+BEGIN_RCPP
     int status = 0;
-    int nargs;
-    char **args;
     
-    PROTECT(_args = AS_CHARACTER(_args));
-    nargs = length(_args) + 1;
-    args = (char **) R_alloc(nargs, sizeof(char *));
+    // Construct argument list: "-dt {float|double}" is required in practice
+    Rcpp::CharacterVector argsR(_args);
+    const int nArgs = argsR.length() + 3;
+    char **args = (char **) R_alloc(nArgs, sizeof(char *));
     args[0] = R_alloc(8, sizeof(char));
+    args[1] = R_alloc(4, sizeof(char));
+    args[2] = R_alloc(8, sizeof(char));
     strcpy(args[0], "niimath");
-    for (int i=1; i<nargs; i++)
+    strcpy(args[1], "-dt");
+    for (int i=3; i<nArgs; i++)
     {
-        const char *element = CHAR(STRING_ELT(_args, i-1));
+        const char *element = argsR[i-3];
         args[i] = R_alloc(strlen(element)+1, sizeof(char));
         strcpy(args[i], element);
     }
     
-    const char *precision = CHAR(STRING_ELT(_precision, 0));
-    if (!strcmp(precision, "single") || !strcmp(precision, "float"))
-        status = main32(nargs, args);
-    else
-        status = main64(nargs, args);
+    // If we have been passed images, retrieve them
+    if (argsR.hasAttribute("images"))
+    {
+        Rcpp::List imageList(argsR.attr("images"));
+        int nImages = imageList.length();
+        if (nImages > 0)
+        {
+            nifti_image **images = (nifti_image **) R_alloc(nImages, sizeof(nifti_image*));
+            for (int i=0; i<nImages; i++)
+            {
+                // Each nifti_image cannot be owned by its NiftiImage, because
+                // niimath handles deallocation, so we have to copy it manually
+                SEXP imagePtr = imageList[i];
+                RNifti::NiftiImage image(imagePtr, true, true);
+                images[i] = nifti_copy_nim_info(image);
+                if (!image.data().isEmpty())
+                {
+                    size_t dataSize = nifti_get_volsize(image);
+                    images[i]->data = calloc(1, dataSize);
+                    memcpy(images[i]->data, image->data, dataSize);
+                }
+            }
+            setInputImages(images, nImages);
+        }
+    }
     
-    UNPROTECT(1);
-    return ScalarInteger(status);
+    // At the precision argument and run the pipeline
+    std::string precision = Rcpp::as<std::string>(_precision);
+    if (precision == "single" || precision == "float")
+    {
+        strcpy(args[2], "float");
+        status = main32(nArgs, args);
+    }
+    else
+    {
+        strcpy(args[2], "double");
+        status = main64(nArgs, args);
+    }
+    
+    // Capture the output and return
+    nifti_image *outputImage = getOutputImage();
+    RNifti::NiftiImage result(outputImage);
+    return result.toPointer("NIfTI image");
+END_RCPP
 }
 
+// C linkage for R
+extern "C" {
+
+// R interface metadata
 static R_CallMethodDef callMethods[] = {
     { "run",    (DL_FUNC) &run,     2 },
     { NULL, NULL, 0 }
 };
 
+// Package initialisation
 void R_init_niimath (DllInfo *info)
 {
    R_registerRoutines(info, NULL, callMethods, NULL, NULL);
    R_useDynamicSymbols(info, FALSE);
    R_forceSymbols(info, TRUE);
 }
+
+} // extern "C"
