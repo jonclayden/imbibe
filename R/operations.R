c3f234c3a84962cbd7211571d87f8725152fb9a0
Jon Clayden <code@clayden.org>
2020-09-28 14:46:46 +0100

    Splitting core pipeline code into a separate file; other tweaks

--- R/operations.R
+++ R/operations.R
@@ -1,65 +1,3 @@
-#' Create an operation pipeline
-#' 
-#' @param image An image object or existing pipeline.
-#' @param x An \code{"imbibe"} object.
-#' @param ... Additional arguments to \code{\link{run}}.
-#' 
-#' @export
-imbibe <- function (image) {
-    if (inherits(image, "imbibe"))
-        image
-    else
-        structure("#1", images=list(asNifti(image,internal=TRUE)), class="imbibe")
-}
-
-.command <- function (init, flag, ...) {
-    init <- imbibe(init)
-    elements <- as.character(c(init, flag))
-    images <- attr(init, "images")
-    args <- list(...)
-    for (i in seq_along(args)) {
-        if (is.numeric(args[[i]]))
-            elements <- c(elements, paste(as.character(args[[i]]), collapse=" "))
-        else {
-            elements <- c(elements, paste0("#",length(images)+1))
-            images <- c(images, list(asNifti(args[[i]],internal=TRUE)))
-        }
-    }
-    structure(elements, images=images, class="imbibe")
-}
-
-
-#' Run a pipeline and return an image result
-#' 
-#' @param pipe An operation pipeline.
-#' @param precision The internal precision used for calculations. \code{"float"} and
-#'   \code{"single"} are equivalent.
-#' @return An image
-#' 
-#' @export
-run <- function (pipe, precision = c("double","float","single")) {
-    precision <- match.arg(precision)
-    .Call(C_run, pipe, precision)
-}
-
-
-#' @rdname imbibe
-#' @export
-asNifti.imbibe <- function (x, ...) {
-    run(x, ...)
-}
-
-
-#' @rdname imbibe
-#' @export
-print.imbibe <- function (x, ...) {
-    if (getOption("imbibe.autorun", TRUE))
-        run(x, ...)
-    else
-        structure(paste(x, collapse=" "), images=attr(x,"images"))
-}
-
-
 #' Basic binary operations
 #' 
 #' @param image An image object or pipeline.
@@ -103,6 +41,7 @@ minimum <- function (image, arg)    .command(image, "-min", arg)
 #' 
 #' @export threshold threshold_below threshold_above
 threshold <- function (image, value, reference = c("none","image","nonzero"), above = FALSE) {
+    reference <- match.arg(reference)
     if (above)
         .command(image, switch(reference,none="-uthr",image="-uthrp",nonzero="-uthrP"))
     else
---
+++ R/pipeline.R
@@ -0,0 +1,64 @@
+#' Create an operation pipeline
+#' 
+#' @param image An image object or existing pipeline.
+#' @param x An \code{"imbibe"} object.
+#' @param ... Additional arguments to methods.
+#' 
+#' @export
+imbibe <- function (image) {
+    if (inherits(image, "imbibe"))
+        image
+    else
+        structure("#1", images=list(asNifti(image,internal=TRUE)), class="imbibe")
+}
+
+.command <- function (init, flag, ...) {
+    init <- imbibe(init)
+    elements <- as.character(c(init, flag))
+    images <- attr(init, "images")
+    args <- list(...)
+    for (i in seq_along(args)) {
+        if (is.numeric(args[[i]]))
+            elements <- c(elements, paste(as.character(args[[i]]), collapse=" "))
+        else {
+            elements <- c(elements, paste0("#",length(images)+1))
+            images <- c(images, list(asNifti(args[[i]],internal=TRUE)))
+        }
+    }
+    structure(elements, images=images, class="imbibe")
+}
+
+
+#' Run a pipeline and return an image result
+#' 
+#' @param pipe An operation pipeline.
+#' @param precision The internal precision used for calculations. \code{"float"} and
+#'   \code{"single"} are equivalent.
+#' @return An image
+#' 
+#' @export
+run <- function (pipe, precision = c("double","float","single")) {
+    precision <- match.arg(precision)
+    .Call(C_run, pipe, precision)
+}
+
+
+#' @rdname imbibe
+#' @export
+asNifti.imbibe <- function (x, ...) {
+    asNifti(run(x), ...)
+}
+
+
+#' @rdname imbibe
+#' @export
+as.array.imbibe <- function (x, ...) {
+    as.array(run(x))
+}
+
+
+#' @rdname imbibe
+#' @export
+print.imbibe <- function (x, ...) {
+    structure(paste(x, collapse=" "), images=attr(x,"images"))
+}
