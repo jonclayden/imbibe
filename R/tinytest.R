64da2fdbc18929e13d05f87eb5b3880c8b5dbf69
Jon Clayden <code@clayden.org>
2020-10-02 18:23:33 +0100

    Adding tinytest custom expectation

---
+++ R/tinytest.R
@@ -0,0 +1,29 @@
+.attribfilter <- function (x) {
+    for (hidden in grep("^\\.", names(attributes(x)), value=TRUE))
+        attr(x, hidden) <- NULL
+    return (x)
+}
+
+#' Expectation for testing pipeline output
+#' 
+#' This function provides an expectation for use with the "tinytest" package,
+#' which runs the pipeline specified in its first argument and compares the
+#' result to its second.
+#' 
+#' @param current The pipeline to run, which should have class \code{"imbibe"}.
+#' @param target The target value to compare against, a numeric array of some
+#'   kind, which will be converted to a \code{"niftiImage"} object.
+#' @param ... Further arguments to \code{expect_equal}.
+#' @return A \code{"tinytest"} object.
+#' 
+#' @rdname tinytest
+#' @export
+expect_pipeline_result <- function (current, target, ...) {
+    if (!inherits(current, "imbibe"))
+        stop("Pipeline under test does not appear to be valid")
+    call <- sys.call(sys.parent(1))
+    current <- .attribfilter(as.array(run(current)))
+    target <- .attribfilter(as.array(asNifti(target)))
+    result <- tinytest::expect_equal(current, target, ...)
+    return (structure(result, call=call))
+}
--- R/zzz.R
+++ R/zzz.R
@@ -6,3 +6,10 @@ NULL
 
 #' @export
 magrittr::`%>%`
+
+.onLoad <- function (libname, pkgname) {
+    # Register tinytest extension if the package is installed
+    if (system.file(package="tinytest") != "") {
+        tinytest::register_tinytest_extension("imbibe", "expect_pipeline_result")
+    }
+}
